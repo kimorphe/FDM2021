2cc7e31db7ad37a1c85423f7bc159f691586cf4b
kazushi <kimoto@okayama-u.ac.jp>
2022-01-29 15:38:22 +0900

    Regular file update

--- FDM/bscan.py
+++ FDM/bscan.py
@@ -1,6 +1,7 @@
 #! /home/kazushi/anaconda3/bin/python
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 
 class REC:
@@ -10,6 +11,7 @@ class REC:
         tmp=fp.readline().lstrip().split(" ");
         self.dt=float(tmp[0]);
         self.Nt=int(tmp[1]);
+        self.time=self.dt*np.array(range(self.Nt))
         fp.readline();
         tmp=fp.readline().lstrip().split(" ");
         self.Ng=int(tmp[0]);
@@ -50,10 +52,54 @@ class REC:
         t1=0.;
         t2=self.dt*self.Nt;
         im=ax.imshow(self.dat,extent=[t1,t2,x1,x2],cmap=cmap,aspect="auto",vmin=v1,vmax=v2,interpolation="bicubic",origin="lower");
+    def get_amp(self,rnum,time):
+        it=int(time/self.dt)
+        err=False
+        if rnum <0:
+            err=True
+        if rnum >self.Ng:
+            err=True
+        if err:
+            print("Invalid receiver number given to get_amp ",rnum)
+            return(0)
+
+        if it <0:
+            return(0)
+        if it >= self.Nt:
+            return(0)
+
+        return(self.dat[rnum,it])
+    def delay_sum(self,cR=2.8): 
+        ydat=np.zeros(self.Nt)
+        for k in range(self.Ng):
+            Tdiff=(self.xsrc[k]-self.xsrc[0])/cR
+            for m in range(self.Nt):
+                tt=m*self.dt+Tdiff
+                ydat[m]+=self.get_amp(k,tt)
+        self.Ysum=ydat/self.Ng;
 
 if __name__=="__main__":
-    rec=REC("rec0.out")
+
+
+    fname="rec0.out"
+    dir_name="."
+
+    narg=len(sys.argv)
+    if narg>1:
+        dir_name=sys.argv[1]
+    if narg>2:
+        fname=sys.argv[2]
+
+    rec=REC(dir_name+"/"+fname)
     fig,ax=rec.bscan();
+
+    rec.delay_sum(cR=2.830)
+    
+    fig2=plt.figure()
+    ax=fig2.add_subplot(111)
+    ax.plot(rec.time,rec.dat[0,:],"-b")
+    ax.plot(rec.time,rec.Ysum,"-r")
+    ax.grid(True)
     plt.show()
 
 
